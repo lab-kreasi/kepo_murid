be058d93355563e4e049feb71a09b3c1fe66b67d
lab-kreasi <labkreasiguru@gmail.com>
2026-08-07 10:04:56 +0800

    Create config.js with application settings
    Added global configuration for KEPO Murid application.

---
+++ js/config.js
@@ -0,0 +1,10 @@
+/**
+ * Konfigurasi Global Aplikasi KEPO Murid
+ */
+const CONFIG = {
+  // Ganti URL di bawah ini dengan URL Web App dari Deploy Google Apps Script Anda
+  API_URL: "https://script.google.com/macros/s/AKfycbz0DLwX_mSpg6HT_pRom8RUJCCLfxAYKfSipySe_yzRWndKWGZ_aNhIaYeK__iG_m5z/exec",
+  
+  // Tahun Ajaran Aktif Default
+  DEFAULT_TAHUN_AJARAN: "2025-2026"
+};
