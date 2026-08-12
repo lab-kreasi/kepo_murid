b21a59a147acab3034ad66c4d7defa48c70db8a8
lab-kreasi <labkreasiguru@gmail.com>
2026-08-12 10:58:34 +0800

    Add BASE_URL to configuration

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 const CONFIG = {
   // Ganti URL di bawah ini dengan URL Web App dari Deploy Google Apps Script Anda
   API_URL: "https://script.google.com/macros/s/AKfycbz0DLwX_mSpg6HT_pRom8RUJCCLfxAYKfSipySe_yzRWndKWGZ_aNhIaYeK__iG_m5z/exec",
-  
+  BASE_URL: "https://script.google.com/macros/s/AKfycbz0DLwX_mSpg6HT_pRom8RUJCCLfxAYKfSipySe_yzRWndKWGZ_aNhIaYeK__iG_m5z/exec",
   // Tahun Ajaran Aktif Default
   DEFAULT_TAHUN_AJARAN: "2026/2027"
 };
